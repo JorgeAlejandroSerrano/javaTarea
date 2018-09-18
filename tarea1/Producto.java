db28a83c42783f64417989143083538702812bf5
JorgeAlejandroSerrano <42601125+JorgeAlejandroSerrano@users.noreply.github.com>
2018-09-18 02:04:31 -0700

    commit 4

---
+++ tarea1/Producto.java
@@ -0,0 +1,19 @@
+public class Producto{
+	private String marca;
+	private int costo;
+
+	public String getMarca(){
+		return marca;
+	}
+	public void String setMarca(String marca){
+		this.marca = marca;
+	}
+	public int getCosto(){
+		return costo;
+	}
+	public void int setCosto(int costo){
+		this.costo = costo
+	}
+
+
+}
