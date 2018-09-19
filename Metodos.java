ca4ee3def54c8a1b905202119c405e2a5b03a2ba
JorgeAlejandroSerrano <42601125+JorgeAlejandroSerrano@users.noreply.github.com>
2018-09-19 01:40:09 -0700

    TAREA 2

---
+++ Metodos.java
@@ -0,0 +1,26 @@
+public class Metodos{
+
+  public int sumar (int n1 , int n2){
+  int resultado = 0;
+  resultado = n1 + n2;
+  return resultado;
+  }
+  public int restar (int n1 , int n2){
+  int resultado = 0;
+  resultado = n1 - n2;
+  return resultado;
+  }
+  public int multiplicar (int n1 , int n2){
+  int resultado = 0;
+  resultado = n1 * n2;
+  return resultado;
+  }
+  public int dividir (int n1 , int n2){
+  int resultado = 0;
+  resultado = (int) (n1*1.0/n2);
+  return resultado;
+  }
+  //public double angulo(){
+    //return Math.atan2(this.imag,this.real);
+  //}
+}
